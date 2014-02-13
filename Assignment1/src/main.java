5da1cd2bde5694382563f768677bd9c98e4455d2
Quinten Stokkink <goqs@hotmail.com>
2014-02-13 16:25:35 +0100

    Updated main.java

--- Assignment1/src/main.java
+++ Assignment1/src/main.java
@@ -1,4 +1,7 @@
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.Map;
 
 import org.da.ass1.ConfigReader;
@@ -14,13 +17,19 @@ public class main implements GenericMessageListener{
 	/**
 	 * @param args
 	 * @throws FileNotFoundException 
+	 * @throws NotBoundException 
+	 * @throws RemoteException 
+	 * @throws MalformedURLException 
 	 */
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, MalformedURLException, RemoteException, NotBoundException {
+		java.rmi.registry.LocateRegistry.createRegistry(1099);
+		
 		Long ourid = Long.parseLong(args[0]);
 		Map<Long, RemoteHost> hosts = new ConfigReader().read();
 		RemoteHost me = hosts.get(ourid);
 		
 		Connector c = new Connector();
+		c.setIndex(hosts);
 		c.subscribe(new main());
 		
 		System.out.println("INITIALIZING REMOTE INVOCATION AND SLEEPING FOR 5 SECONDS");
@@ -42,8 +51,7 @@ public class main implements GenericMessageListener{
 
 	@Override
 	public long getProcessId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 
 }
