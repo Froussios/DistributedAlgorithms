8fc88be763a6b4f113fca11694671e2c92c32669
Quinten Stokkink <goqs@hotmail.com>
2014-02-13 16:01:31 +0100

    Addded some main testing methods

--- Assignment1/src/main.java
+++ Assignment1/src/main.java
@@ -1,12 +1,37 @@
+import java.io.FileNotFoundException;
+import java.util.Map;
 
-public class main {
+import org.da.ass1.ConfigReader;
+import org.da.ass1.Connector;
+import org.da.ass1.GenericMessageListener;
+import org.da.ass1.RemoteHost;
+import org.da.ass1.messages.GenericMessage;
+
+
+public class main implements GenericMessageListener{
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		Long ourid = Long.parseLong(args[0]);
+		Map<Long, RemoteHost> hosts = new ConfigReader().read();
+		RemoteHost me = hosts.get(ourid);
+		
+		Connector c = new Connector();
+		c.subscribe(new main());
+	}
 
+	@Override
+	public void receive(GenericMessage gm, long fromProcess) {
+		System.out.println("RECEIVED GENERIC MESSAGE " + gm.toString());
+	}
+
+	@Override
+	public long getProcessId() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
