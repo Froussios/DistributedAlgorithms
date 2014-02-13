46c96fd97d3967327a18be685c9b9d15678334d0
Chris Froussios <darkmatter.tm@gmail.com>
2014-02-13 15:50:20 +0100

    Implemented RMI receiver

--- Assignment1/src/org/da/ass1/connector/RMIReceiver.java
+++ Assignment1/src/org/da/ass1/connector/RMIReceiver.java
@@ -1,31 +1,54 @@
 package org.da.ass1.connector;
 
+import java.net.MalformedURLException;
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 
+import org.da.ass1.Connector;
 import org.da.ass1.messages.*;
 
+
+/**
+ * The implementation of the remote interface for  receiving messages
+ * @author Chris
+ *
+ */
 public class RMIReceiver
 	extends UnicastRemoteObject
 	implements IRMIConnector
 {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
-	protected RMIReceiver(/*insert listener here*/) throws RemoteException
+	private Connector connector;
+	
+	
+	/**
+	 * 
+	 * @param connector The instance that will handle the reception of a message
+	 * @param urlname The url this instace will be responding to
+	 * @throws RemoteException
+	 * @throws MalformedURLException url parameter is malformed
+	 * @throws AlreadyBoundException url location is already used
+	 */
+	protected RMIReceiver(String urlname, Connector connector) throws RemoteException, MalformedURLException, AlreadyBoundException
 	{
 		super();
-		// TODO Auto-generated constructor stub
 		
-		//java.rmi.Naming.bind("rmi://localhost:1201/object-name", this);
+		this.connector = connector;
+		
+		java.rmi.Naming.bind(urlname, this);
 	}
 
-	public void receive(GenericMessage message) throws RemoteException 
+	
+	/**
+	 * The callee receives a message
+	 * @param caller The id of the process sending the message
+	 * @param message The message to be sent
+	 * @throws RemoteException
+	 */
+	public void receive(long caller, GenericMessage message) throws RemoteException 
 	{
-		// TODO Auto-generated method stub
-		
+		connector.receive(caller, message);
 	}
 
 }
