f3604f1b9d766315b1763df1494fac006d430dd6
Quinten Stokkink <goqs@hotmail.com>
2014-04-28 13:44:54 +0200

    Implemented candidate process

--- Assignment3/src/org/da/ass3/CandidateProcess.java
+++ Assignment3/src/org/da/ass3/CandidateProcess.java
@@ -5,6 +5,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.da.ass3.messages.Acknowledgement;
 import org.da.ass3.messages.CandidateMessage;
@@ -48,19 +49,64 @@ public class CandidateProcess extends Thread implements GenericMessageListener {
 		alive = false;
 	}
 
+	private ConcurrentLinkedQueue<MsgTuple> messageQueue = new ConcurrentLinkedQueue<MsgTuple>();
+	private ConcurrentLinkedQueue<Long> untraversed = new ConcurrentLinkedQueue<Long>();
+	private int level = -1;
+	private int id = -1;
+	private boolean killed = false;
+	private boolean elected = false;
+	
 	@Override
 	public void run(){
-		int K = 0;
-		
-		while (alive){
-			
+		while (alive && !untraversed.isEmpty()){
+			long link = untraversed.poll();
+			try {
+				connector.send(link, new CandidateMessage(level, id));
+			} catch (MalformedURLException | RemoteException
+					| NotBoundException e) {
+				e.printStackTrace();
+			}
+			boolean R = true;
+			while (R){
+				R = false;
+				while ((alive && messageQueue.isEmpty()) || (alive && messageQueue.peek().getLink() != link)){
+					// Wait
+					try { Thread.sleep(5); } catch (InterruptedException e) {}
+				}
+				MsgTuple message = messageQueue.poll();
+				if (message.getId() == id && !killed){
+					level++;
+				} else {
+					if (message.compareTo(new MsgTuple(level, id)) < 0){
+						// Goto R
+						R = true;
+					} else {
+						try {
+							connector.send(link, new CandidateMessage(message.getLevel(), message.getId()));
+						} catch (MalformedURLException | RemoteException
+								| NotBoundException e) {
+							e.printStackTrace();
+						}
+						killed = true;
+						// Goto R
+						R = true;
+					}
+				}
+			}
 		}
+		if (!killed)
+			elected = true;
 	}
 
 	@Override
 	public void receive(GenericMessage gm, long fromProcess)
 			throws MalformedURLException, RemoteException, NotBoundException {
-		
+		if (gm instanceof CandidateMessage)
+			messageQueue.add(new MsgTuple((CandidateMessage) gm, fromProcess));
+	}
+	
+	public boolean isElected() {
+		return elected;
 	}
 
 	@Override
