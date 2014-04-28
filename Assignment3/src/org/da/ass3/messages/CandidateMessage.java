d30d97eadababc207dfb479192009b1f362dccbc
Chris Froussios <darkmatter.tm@gmail.com>
2014-04-28 14:19:14 +0200

    Message to string

--- Assignment3/src/org/da/ass3/messages/CandidateMessage.java
+++ Assignment3/src/org/da/ass3/messages/CandidateMessage.java
@@ -1,23 +1,39 @@
 package org.da.ass3.messages;
 
-public class CandidateMessage extends GenericMessage implements Comparable<GenericMessage>{
+public class CandidateMessage
+		extends GenericMessage
+		implements Comparable<GenericMessage>
+{
 
 	private static final long serialVersionUID = 1L;
 
 	private final int level;
 	private final long id;
-	
-	public CandidateMessage(int level, long id){
+
+
+	public CandidateMessage(int level, long id)
+	{
 		this.level = level;
 		this.id = id;
 	}
-	
-	public int getLevel(){
+
+
+	public int getLevel()
+	{
 		return level;
 	}
-	
-	public long getId(){
+
+
+	public long getId()
+	{
 		return id;
+	}
+
+
+	@Override
+	public String toString()
+	{
+		return this.getClass().getSimpleName() + ": Level " + level + ", Id " + id;
 	}
 
 }
